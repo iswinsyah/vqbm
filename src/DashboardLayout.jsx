16a4d08837e53ac7972fb373ad5d08e5b82e7840
Iswinsyah <iswinsyah@example.com>
2026-03-07 05:53:58 +0700

    Menambahkan menu Testimoni di sidebar admin

--- src/DashboardLayout.jsx
+++ src/DashboardLayout.jsx
@@ -10,6 +10,7 @@ const DashboardLayout = () => {
           <Link to="/admin" className="block py-3 px-6 hover:bg-gray-800 border-l-4 border-transparent hover:border-blue-500">Dashboard</Link>
           <Link to="/admin/posts" className="block py-3 px-6 hover:bg-gray-800 border-l-4 border-transparent hover:border-blue-500">Semua Post</Link>
           <Link to="/admin/pages" className="block py-3 px-6 hover:bg-gray-800 border-l-4 border-transparent hover:border-blue-500">Semua Halaman</Link>
+          <Link to="/admin/testimonials" className="block py-3 px-6 hover:bg-gray-800 border-l-4 border-transparent hover:border-blue-500">Testimoni</Link>
           <Link to="/admin/menus" className="block py-3 px-6 hover:bg-gray-800 border-l-4 border-transparent hover:border-blue-500">Pengaturan Menu</Link>
           <Link to="/admin/settings" className="block py-3 px-6 hover:bg-gray-800 border-l-4 border-transparent hover:border-blue-500">Pengaturan Situs</Link>
           <div className="mt-6 pt-6 border-t border-gray-800">
